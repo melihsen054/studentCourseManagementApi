736398e88778a04c541785c897233e4085b3f518
melihsen <melihsensakarya@gmail.com>
2021-09-13 14:10:37 +0300

    Initial files added

---
+++ src/main/java/com/melihsen/studentCourseManagementApi/StudentCourseManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.melihsen.studentCourseManagementApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCourseManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCourseManagementApiApplication.class, args);
+	}
+
+}
